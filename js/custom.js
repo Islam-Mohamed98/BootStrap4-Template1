63e77d26959a6c3bb59f257c23fbd557e724fa4c
eslam <eslam@yahoo.com>
2018-09-20 00:49:45 +0300

    Shuffle images + Some Fixes

--- js/custom.js
+++ js/custom.js
@@ -10,4 +10,15 @@ $(function () {
 		navH = $('.navbar').innerHeight();
 		$('.slider, .carousel-item').height( winH - navH);
 		});
+
+	// Features-work Shuffle
+	$('.features-work ul li').click(function () {
+		$(this).addClass('active').siblings().removeClass('active');
+		if ($(this).data('class') === 'all') {
+			$('.features-work .col-md').css('opacity','1');
+		} else {
+			$('.features-work .col-md').css('opacity','.08');
+			$($(this).data('class')).parent().css('opacity','1');
+		}
+	});
 });
